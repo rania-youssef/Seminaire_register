f51f06d0dd561a7cc03d7d9df9d20215c5e0e80a
Rania Youssef <raniay089@gmail.com>
2022-03-31 22:38:51 +0100

    third version

--- public/index.php
+++ public/index.php
@@ -5,9 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-    }
+   
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
     
 };
